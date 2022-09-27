9fa13434e6db8c89ec03457704ed07d81c6fb789
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-27 18:43:43 +0200

    feat: initial route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
 
 Auth::routes();
 
@@ -39,3 +36,7 @@ Route::middleware('auth')
 
 });
 // Route::middleware('auth')->resource('/posts', 'PostsController');
+
+Route::get("{any?}", function() {
+    return view("guest.home");
+})->where("any", ".*");
