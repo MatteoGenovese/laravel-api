9117a85bf4dd56a6543270a52f967fabf98fee22
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-27 18:43:17 +0200

    fix: api instead Api

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route:: namespace( 'Api')->group(function(){
+Route:: namespace( 'api')->group(function(){
     Route::get('/posts', 'PostController@index');
     Route::get('/posts/{id}', 'PostController@show');
     Route::middleware('auth:api')->delete('/posts/{id}', 'PostController@destroy');
-
-
 });
+
+
