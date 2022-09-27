e4428ce745f0e130fdb1f8b0e18fe639ccc605c3
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-27 18:41:28 +0200

    feat: card component

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="col-12">
+
+
+    <div class="card">
+        <div class="card-title">
+            {{ post.title }}
+        </div>
+
+    </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    props:['post']
+
+
+};
+</script>
+
+<style>
+
+</style>
